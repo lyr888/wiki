cc33f74df13d0aa453f48b02ce4ea38c498f0627
lyr <liyingrui19981104@163.com>
2022-12-23 19:43:43 +0800

    增加了Springboot启动项配置日志

--- src/main/java/com/example/wiki_project/WikiProjectApplication.java
+++ src/main/java/com/example/wiki_project/WikiProjectApplication.java
@@ -14,10 +14,8 @@ public class WikiProjectApplication {
     private static final Logger logger=Logger.getLogger(WikiProjectApplication.class.getName());
     public static void main(String[] args) {
         SpringApplication app = new SpringApplication(WikiProjectApplication.class);
-//        SpringApplication.run(WikiProjectApplication.class, args);
         Environment env = app.run(args).getEnvironment();
         logger.info("启动成功");
-//        logger.info("地址：\t http://127.0.0.1:{}",env.getProperty("server.port"));
 
     }
 
